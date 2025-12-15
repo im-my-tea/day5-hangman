dbbd0962028fb13a91bb80aa8cde11f16032325e
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-15 23:53:03 +0530

    Game logic part 1

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ for i in chosen_word:
     display += "_"
 print(display)
 print(" !! " + chosen_word)
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for i in range(len(chosen_word)):
+        letter = chosen_word[i]
+        if letter == guess:
+            display[i] = letter
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
