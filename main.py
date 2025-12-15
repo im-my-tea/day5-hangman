492fd3b0dc13e00a7d2a26c81911c29556ca8df1
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-16 00:05:13 +0530

    Full game

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ print(display)
 print(" !! " + chosen_word)
 
 end_of_game = False
+lives = 6
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
     for i in range(len(chosen_word)):
         letter = chosen_word[i]
         if letter == guess:
             display[i] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Your guess {guess} is not in the word. You lose a life. Lives left: {lives}")
+    if lives == 0:
+        end_of_game = True
+        print("You lose")
+        break
     print(display)
     if "_" not in display:
         end_of_game = True
         print("You win")
+        break
+    print(art.stages[lives])
